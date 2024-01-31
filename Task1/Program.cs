dd2d8d1c604d262da9dc9bfe10798edb54762449
“iamIlyaAlex” <blagas77@me.com>
2024-01-31 16:29:36 +0300

    added a solution to the Task

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        PrintNumbersInRange(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbersInRange(m + 1, n);
+        }
+    }
+}
