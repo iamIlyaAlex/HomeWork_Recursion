99cbcbca46f9a6447d89644b2d3c643291684c3a
“iamIlyaAlex” <blagas77@me.com>
2024-02-01 05:35:03 +0300

    added a solution to the Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            if (index != arr.Length - 1)
+            {
+                Console.Write(", ");
+            }
+            Console.Write(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 11, 22, 13, 14, 35 };
+        
+        Console.Write("Массив: [");
+        PrintArrayReverse(arr, arr.Length - 1);
+        Console.WriteLine("]");
+    }
+}
